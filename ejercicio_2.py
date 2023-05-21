be6c29d1fc9ccc515593acb9885aaf0477499ce9
Camilo Andres Fernandez <123255274+fernandezcamiloa@users.noreply.github.com>
2023-05-21 13:58:10 -0300

    Delete ejercicio_2.py

--- ejercicio_2.py
+++
@@ -1,5 +0,0 @@
-#pensarlos de manera recursiva: se puede hacer hasta el 6.
-
-#Implementar una función que calcule la suma de todos los números enteros comprendidos
-#entre cero y un número entero positivo dado.
-
